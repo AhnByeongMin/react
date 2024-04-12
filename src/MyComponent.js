567e3add53fa14aa4a83c70092b9f56990007ba4
haruittl <qudals0617@naver.com>
2024-04-12 14:59:17 +0900

    비구조화 할당으로 props 내부 값 추출

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,13 +1,14 @@
 // =>는  ES6문법
 //defaultProps 지정
-const MyComponent = (props) => {
+const MyComponent = props => {
+    const {name, children, nick} = props;
   return (
     <>
       <div>
         <h1>나의 새로운 컴포넌트!</h1>
-        <h2>안녕, 난 {props.name}이야!</h2>
-        <h2>그리고 지정하지 않은 기본 닉네임은 {props.nick}이야!</h2>
-        <h2>컴포넌트 태그 안 children 값은 {props.children}이야</h2>
+        <h2>안녕, 난 {name}이야!</h2>
+        <h2>그리고 지정하지 않은 기본 닉네임은 {nick}이야!</h2>
+        <h2>컴포넌트 태그 안 children 값은 {children}이야</h2>
       </div>
     </>
   );
