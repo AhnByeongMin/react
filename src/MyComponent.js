0e73fea5e8b7f65e6dd58f42562a28ad3b274038
haruittl <qudals0617@naver.com>
2024-04-12 14:56:37 +0900

    태그내용사이를 보여주는 children

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -7,6 +7,7 @@ const MyComponent = (props) => {
         <h1>나의 새로운 컴포넌트!</h1>
         <h2>안녕, 난 {props.name}이야!</h2>
         <h2>그리고 지정하지 않은 기본 닉네임은 {props.nick}이야!</h2>
+        <h2>컴포넌트 태그 안 children 값은 {props.children}이야</h2>
       </div>
     </>
   );
