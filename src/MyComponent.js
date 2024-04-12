b0e9ca96b149962acd85bc35bf3abeacef6c0c50
haruittl <qudals0617@naver.com>
2024-04-12 14:53:12 +0900

    컴포넌트 props 지정과 디폴트

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,14 +1,19 @@
 // =>는  ES6문법
-// 
-const MyComponent = props => {
+//defaultProps 지정
+const MyComponent = (props) => {
   return (
     <>
       <div>
         <h1>나의 새로운 컴포넌트!</h1>
         <h2>안녕, 난 {props.name}이야!</h2>
-        </div>
+        <h2>그리고 지정하지 않은 기본 닉네임은 {props.nick}이야!</h2>
+      </div>
     </>
   );
+};
+MyComponent.defaultProps = {
+  name: "기본이름",
+  nick :"리액트"
 };
 
 export default MyComponent;
