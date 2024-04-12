cf3f03b17f1a1e55d93b1cd8fe8d30b62504a135
haruittl <qudals0617@naver.com>
2024-04-12 22:46:31 +0900

    useState 여러개쓰기

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Say from "./Say";
 const App = () => {
   return (
     <>
-    <MyComponent name ={3}>태그내용</MyComponent>
-    <Counter />
-    <Say />
+    <MyComponent name ={3}>태그내용</MyComponent><br/>
+    <Counter /><br/>
+    <Say /><br/>
     </>
 )
 
