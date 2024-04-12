21dbedd6f97c3e3cf3f748c499f5b2fca62f44ab
haruittl <qudals0617@naver.com>
2024-04-12 14:48:14 +0900

    모듈 불러오기 및 컴포넌트 prps 값 지정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import { Component } from "react";
-// 클래스형 컴포넌트 
-class App extends Component {
-  render() {
-    const name = 'react';
-    return <div className="react">{name}</div>;
-  }
-}
+import MyComponent from "./MyComponent";
+
+const App = () => {
+  return <MyComponent name ="React" />
+
+};
 
 export default App;
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,6 +1,14 @@
 // =>는  ES6문법
-const MyComponent = () => {
-  return <div>나의 새로운 컴포넌트!</div>;
+// 
+const MyComponent = props => {
+  return (
+    <>
+      <div>
+        <h1>나의 새로운 컴포넌트!</h1>
+        <h2>안녕, 난 {props.name}이야!</h2>
+        </div>
+    </>
+  );
 };
 
 export default MyComponent;
