38d6a2ecff3952dfc6f86d451698c38dd9a67760
haruittl <qudals0617@naver.com>
2024-04-12 13:46:20 +0900

    ESLine과 prettier 설치로 정렬

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  const name = '리액트';
+  const name = "리액트";
   // 기존에 html 테그 안에서 class 로 css를 적용했던것 대신 className을 통해 적용시킨다.
   return (
-      <>
-        {/*
+    <>
+      {/*
         테그는 꼭 닫아주도록 한다. ex <input></input> 테그사이 내용이 없는경우 셀프클로징 <input />
         */}
-        <div className='react'>
-          {name || '리액트'}
-        </div>
-          <input/>
-      </>
+      <div className="react">{name || "리액트"}</div>
+      <h1>들여쓰기를</h1>
+      <h2>이상하게 해보자</h2>
+      <input />
+    </>
   );
 }
 
