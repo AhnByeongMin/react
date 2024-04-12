aa0854b638e8857188520eae0f9c5e355c72ef7c
haruittl <qudals0617@naver.com>
2024-04-12 13:32:37 +0900

    JSX에서 테그 클로징

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ function App() {
   const name = '리액트';
   // 기존에 html 테그 안에서 class 로 css를 적용했던것 대신 className을 통해 적용시킨다.
   return (
-    <div className='react'>
-      {name || '리액트'}
-    </div>
+      <>
+        {/*
+        테그는 꼭 닫아주도록 한다. ex <input></input> 테그사이 내용이 없는경우 셀프클로징 <input />
+        */}
+        <div className='react'>
+          {name || '리액트'}
+        </div>
+          <input/>
+      </>
   );
 }
 
