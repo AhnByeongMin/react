b6acba7c77e95dd163358b1c53d9c65cdac785e4
haruittl <qudals0617@naver.com>
2024-04-12 12:02:30 +0900

    삼항연산자 조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const name = '리웩트';
   return (
     <div className="App">
-      {name === '리액트' ? <h1>리액트 입니다.</h1> : <h1>리액트가 아닙니다.</h1>}
+      {name === '리액트' ? <h1>리액트 입니다.</h1> : null}
     </div>
   );
 }
