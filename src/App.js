d5e8e66d1b8ca8f02612e594eae979e987f22122
haruittl <qudals0617@naver.com>
2024-04-12 11:55:05 +0900

    if문대신 조건부연산자

--- src/App.js
+++ src/App.js
@@ -5,26 +5,7 @@ function App() {
   const name = '리웩트';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {name == '리엑트' ?(
-            <h1>리엑트 입니다.</h1>
-        ) : (
-            <h1>리엑트가 아닙니다.</h1>
-        )}
-	  
-      </header>
+      {name === '리액트' ? <h1>리액트 입니다.</h1> : <h1>리액트가 아닙니다.</h1>}
     </div>
   );
 }
