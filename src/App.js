4629e51e4aae3d91b2259e90548b14f2f67d3aa9
haruittl <qudals0617@naver.com>
2024-04-12 12:08:01 +0900

    AND 연산자(&&)를 사용한 조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const name = '리웩트';
+  const name = '리액트';
   return (
     <div className="App">
-      {name === '리액트' ? <h1>리액트 입니다.</h1> : null}
+      {/*
+      && 연산자로 인해 name이 '리액트'로 true이기에 뒤의 h1테그가 랜더링
+      반대로 AND조건이기때문에 name이 false일경우 AND조건에 맞지않아 뒷내용 랜더링되지않음
+      */}
+      {name === '리액트' && <h1>리액트 입니다.</h1>}
     </div>
   );
 }
