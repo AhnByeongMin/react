9705d5f000b6039d5e2e85e7ac24414cf59257df
haruittl <qudals0617@naver.com>
2024-04-12 12:39:55 +0900

    undefined를 렌더링하지 않기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 
 function App() {
-  const name = '리액트';
+  const name = undefined;
   return (
-    <div className="App">
+    <div>
       {/*
-      && 연산자로 인해 name이 '리액트'로 true이기에 뒤의 h1테그가 랜더링
-      반대로 AND조건이기때문에 name이 false일경우 AND조건에 맞지않아 뒷내용 랜더링되지않음
+      undefined 값을 리턴시 null 포인트 에러가 발생하니까 undefined를 리턴하여 랜더링 하는 일이 없도록 하자.
+      JSX 내부에서는 괜찮다. 값이 undefined일경우에서는 java에서와 같이 널포인트 방지를 해주자.
       */}
-      {name === '리액트' && <h1>리액트 입니다.</h1>}
+      {name || '리액트'}
     </div>
   );
 }
