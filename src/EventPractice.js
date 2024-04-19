379ebf0ebd40ab1aa3d75af7f3c88a258978a361
haruhome <qudals0617@naver.com>
2024-04-19 21:35:46 +0900

    함수형 컴포넌트로 구현/ form객체 활용

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,55 +1,51 @@
-import { Component } from "react";
-class EventPractice extends Component {
-    state = {
+import { useState } from "react";
+const EventPractice = () => {
+    const [form, setForm] = useState({
         username: '',
         message: ''
+    })
+    const { username, message } = form;
+    const onChenge = e => {
+        const nextForm = {
+            ...form, // 기존의 form 내용을 복사
+            [e.target.name]: e.target.value // 원하는 값읖 덮어 씌우기 
+        };
+        setForm(nextForm);
     }
-
-    handleChange = (e) => {
-        this.setState({
-            // 1개일땐 state의 밸류 이름을 직접 사용하지만 여러개일땐 target.nanme을 이용한다.
-            [e.target.name]: e.target.value
-        });
-    }
-
-    handleClick = (e) => {
-        alert(this.state.username + ' : ' + this.state.message);
-        this.setState({
+    const onClick = () => {
+        alert(username + ' : ' + message);
+        setForm({
             username: '',
             message: ''
-        });
+        })
     }
-
-    handleKeyPress = (e) => {
+    const onKetPress = e => {
         if (e.key === 'Enter') {
-            this.handleClick();
+            onClick();
         }
     }
-
-    render() {
-        return (
-            <div>
-                <h1>이벤트 연습</h1>
-                <input
-                    type="text"
-                    name="username"
-                    placeholder="사용자명"
-                    value={this.state.username}
-                    onChange={this.handleChange}
-                />
-                <input
-                    type="text"
-                    name="message"
-                    placeholder="아무거나 입력해 보세요"
-                    value={this.state.message}
-                    onChange={this.handleChange}
-                    onKeyDown={this.handleKeyPress}
-                />
-                <button
-                    onClick={this.handleClick}
-                >확인</button>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <h1>이벤트 연습</h1>
+            <input
+                type="text"
+                name="username"
+                placeholder="사용자명"
+                value={username}
+                onChange={onChenge}
+            />
+            <input
+                type="text"
+                name="message"
+                placeholder="아무거나 입력해 보세요"
+                value={message}
+                onChange={onChenge}
+                onKeyDown={onKetPress}
+            />
+            <button
+                onClick={onClick}
+            >확인</button>
+        </div>
+    )
 }
 export default EventPractice;
