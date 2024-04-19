8d974698fcd057c677f69eb246cb6a2101210419
haruhome <qudals0617@naver.com>
2024-04-19 22:09:07 +0900

    ref DOM 학습을위한 ValidationSample 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import MyComponent from "./MyComponent";
 import Counter from "./Counter";
 import Say from "./Say";
 import EventPractice from "./EventPractice"
+import ValidationSample from "./ValidationSample"
 const App = () => {
   return (
     <>
       <MyComponent name="리액트" check="필수">태그내용</MyComponent><br />
       <Counter /><br />
       <Say /><br />
-      <EventPractice />
+      <EventPractice /><br />
+      <ValidationSample />
     </>
   )
 
